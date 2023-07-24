fb527b53ed36dfc9294642e1801d4d54a66b1be3
Ventsislav Petrov <ventsislav.petrov@lights.digital>
2023-07-25 02:31:22 +0300

    update log message

--- CatsWorld/CatsWorld/Startup.cs
+++ CatsWorld/CatsWorld/Startup.cs
@@ -5,6 +5,6 @@ public class Startup
     public static void Main(string[] args)
     {
         ILogger logger = new ConsoleLogger();
-        logger.Log("Hello, Cats!");
+        logger.Log("Hello, Cats3!");
     }
 }
